c761495d04c4379d567448eccad77ce05effb843
Luis Isea <isea.luis.miguel@gmail.com>
2024-10-17 15:40:31 -0400

    feat(2023): add solution for part one of day two

---
+++ day_two/part_one.py
@@ -0,0 +1,110 @@
+import sys
+
+
+class Game:
+    def __init__(
+        self,
+        id: int,
+        max_red_cubes: int = None,
+        max_green_cubes: int = None,
+        max_blue_cubes: int = None,
+    ):
+        self.id = id
+        self.max_red_cubes = max_red_cubes
+        self.max_green_cubes = max_green_cubes
+        self.max_blue_cubes = max_blue_cubes
+
+    def update_max_red_cubes(self, red_cubes: int):
+        if self.max_red_cubes is None:
+            self.max_red_cubes = red_cubes
+        elif red_cubes > self.max_red_cubes:
+            self.max_red_cubes = red_cubes
+
+    def update_max_green_cubes(self, green_cubes: int):
+        if self.max_green_cubes is None:
+            self.max_green_cubes = green_cubes
+        elif green_cubes > self.max_green_cubes:
+            self.max_green_cubes = green_cubes
+
+    def update_max_blue_cubes(self, blue_cubes: int):
+        if self.max_blue_cubes is None:
+            self.max_blue_cubes = blue_cubes
+        elif blue_cubes > self.max_blue_cubes:
+            self.max_blue_cubes = blue_cubes
+
+    def get_id(self):
+        return self.id
+
+    def get_max_red_cubes(self):
+        return self.max_red_cubes
+
+    def get_max_green_cubes(self):
+        return self.max_green_cubes
+
+    def get_max_blue_cubes(self):
+        return self.max_blue_cubes
+
+    def __str__(self):
+        return f"Game {self.id}: {self.max_red_cubes} red, {self.max_green_cubes} green, {self.max_blue_cubes} blue"
+
+
+def main(print_output: bool = True):
+    try:
+        with open("input.txt") as input_file:
+            lines = input_file.readlines()
+            games: list[Game] = []
+
+            for line in lines:
+                # We create a new game object with the game_id and the max cubes
+                game = Game(
+                    id=int(line.split()[1].removesuffix(":")),
+                )
+
+                # Remove the "Game ID: " part
+                rest_of_line = line.split(": ", 1)[1].removesuffix("\n")
+
+                # Directly divide the rest of the line into a list of cubes divided by ; and then by ,
+                hints = [data.split(", ") for data in rest_of_line.split("; ")]
+
+                # We iterate over the hints and update the max cubes for each color
+                for hint in hints:
+                    for cube in hint:
+                        amount, color = cube.split(" ")
+                        if color == "red":
+                            game.update_max_red_cubes(int(amount))
+                        elif color == "green":
+                            game.update_max_green_cubes(int(amount))
+                        elif color == "blue":
+                            game.update_max_blue_cubes(int(amount))
+
+                # We add the game to the list of games
+                games.append(game)
+
+            valid_games = []
+
+            # We then see which game has less or equal to 12 red cubes, 13 green cubes, and 14 blue cubes
+            for game in games:
+                if (
+                    game.get_max_red_cubes() < 13
+                    and game.get_max_green_cubes() < 14
+                    and game.get_max_blue_cubes() < 15
+                ):
+                    valid_games.append(game)
+
+            # We calculate the sum of the game ids of the valid games
+            game_ids_sum = sum(game.get_id() for game in valid_games)
+
+            if print_output:
+                print(f"Sum of all game IDs: {game_ids_sum}")
+                sys.exit(0)
+
+    except FileNotFoundError:
+        print("The file input.txt was not found.")
+        sys.exit(1)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
